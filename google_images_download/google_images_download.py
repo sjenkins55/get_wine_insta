96116e2ec856be53c07a51e275b1b6c0d6f00e92
sjenkins55 <42223661+sjenkins55@users.noreply.github.com>
2019-03-15 16:19:23 -0500

    Update google_images_download.py

--- google_images_download/google_images_download.py
+++ google_images_download/google_images_download.py
@@ -564,7 +564,7 @@ class googleimagesdownload:
             True
             #print("Image URL: " + image_url)
         if no_download:
-            return "success",None, image_url
+            return "success",None, None, image_url
         try:
             req = Request(image_url, headers={
                 "User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"})
